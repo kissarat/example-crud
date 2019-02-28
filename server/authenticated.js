128f3fd9528aaa40c35d848b241ea8b68952f367
Taras Labiak <kissarat@gmail.com>
2019-02-28 21:04:57 +0200

    + /auth/login

--- server/authenticated.js
+++ server/authenticated.js
@@ -1,15 +1,15 @@
 const query = require("./query");
 const { fail, explain } = require("./responses");
 
+const QUERY = "select usrName, tknID from tblUsers join tblToken on usrID = tkn_usrID where tknID = ?";
+
 module.exports = async function (req, res, next) {
   const m = /\w+$/.exec(req.headers.authentication || '');
   if (!m) {
     return void res.status(401).end();
   }
   try {
-    const [user] = await query("select usrName, tknID from tblUsers join tblToken on usrID = tkn_usrID where tknID = ?", [
-      m[0]
-    ]);
+    const [user] = await query(QUERY, [m[0]]);
     if (user) {
       req.user = user;
       next();
